a061da78099a589254dca14be78e7d80dce722fd
2paperstar <lee@paperst.ar>
2024-06-22 11:39:48 +0900

    fix: auto redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage.tsx';
 import MainPage from './MainPage.tsx';
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import UserProvider from './hooks/UserProvider.tsx';
+import useUser from './hooks/useUser.ts';
 
 const queryClient = new QueryClient();
 
 const Router = () => {
+  const { me } = useUser();
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/user/join" element={<LoginPage />} />
-        <Route path="/" element={<MainPage />} />
-      </Routes>
+      {me ? (
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route element={<Navigate to="/" />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/user/join" element={<LoginPage />} />
+          <Route element={<Navigate to="/user/join" />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 };
