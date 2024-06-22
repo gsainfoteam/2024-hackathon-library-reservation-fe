5a35974d3abc9fd1882560563f328552f12f8fff
2paperstar <lee@paperst.ar>
2024-06-22 12:18:14 +0900

    fix: login type

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,6 +3,6 @@ import api from '.';
 export const login = (code: string) =>
   api
     .get<{ access_token: string }>('user/login', {
-      params: { code, type: 'local' },
+      params: { code, type: import.meta.env.DEV ? 'local' : 'web' },
     })
     .then((res) => res.data.access_token);
